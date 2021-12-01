ef7cd7da8d5911f46daf0e82f4e2b55332c624ef
catherinekasper1998 <44876776+catherinekasper1998@users.noreply.github.com>
2021-12-01 15:34:03 -0500

    wip for temp read through adc (mcp3002)
    finally found a built in library for the adc converter (had to get a new ADC the mcp3002 which I got for free from the hive). So I will likely not need the mcp3002.py file but keeping just in case. Rn I am working to get this temp sensor working with the new library because it just reads zero for the moment.

---
+++ Test_Code/test_temp_only.py
@@ -0,0 +1,69 @@
+'''
+import time
+import mcp3002 as mcp #
+
+def readPotentiometer():
+    global potentiometer
+    potentiometer = mcp.readAnalog() #
+
+def main():
+    while True: #
+        readPotentiometer() #
+        print("The current potentiometer value is %i " % potentiometer) #
+        time.sleep( 0.5) # s
+
+if __name__ = = "__main__":
+    main()
+'''
+import os
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3002 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D8)
+
+# create the mcp object
+mcp = MCP.MCP3002(spi, cs)
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
+
+print('Raw ADC Value: ', chan0.value)
+print('ADC Voltage: ' + str(chan0.voltage) + 'V')
+
+
+def remap_range(value, left_min, left_max, right_min, right_max):
+    # this remaps a value from original (left) range to new (right) range
+    # Figure out how 'wide' each range is
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+
+    # Convert the left range into a 0-1 range (int)
+    valueScaled = int(value - left_min) / int(left_span)
+
+    # Convert the 0-1 range into a value in the right range.
+    return int(right_min + (valueScaled * right_span))
+
+while True:
+
+    # read the analog pin
+    trim_pot = chan0.value
+
+
+    # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
+    set_volume = remap_range(trim_pot, 0, 65535, 0, 65000)
+
+    # set OS volume playback volume
+    print('Volume = {volume}%' .format(volume = set_volume))
+    set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' . format(volume = set_volume)
+    os.system(set_vol_cmd)
+    
+    # hang out and do nothing for a half second
+    time.sleep(0.5)
