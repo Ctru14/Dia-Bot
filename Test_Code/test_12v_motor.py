773f6981defdcc5ee4d874181806572408316123
catherinekasper1998 <44876776+catherinekasper1998@users.noreply.github.com>
2021-12-04 19:32:52 -0500

    Update test_12v_motor.py
    Both motors running with external 18V power supply

--- Test_Code/test_12v_motor.py
+++ Test_Code/test_12v_motor.py
@@ -1,4 +1,4 @@
-  
+ 
 # Python Script
 # https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
 
@@ -7,18 +7,30 @@ from time import sleep
 
 in1 = 24
 in2 = 23
-en = 25
+enA = 25
 temp1=1
+
+in3 = 0
+in4 = 5
+enB = 6
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(enA,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+pA=GPIO.PWM(enA,1000)
 
-p.start(25)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(enB,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+pB=GPIO.PWM(enB,1000)
+
+pA.start(25)
+pB.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -33,11 +45,15 @@ while(1):
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
          print("forward")
          x='z'
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
          print("backward")
          x='z'
 
@@ -46,12 +62,16 @@ while(1):
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
         x='z'
 
     elif x=='f':
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
         temp1=1
         x='z'
 
@@ -59,22 +79,27 @@ while(1):
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
         temp1=0
         x='z'
 
     elif x=='l':
         print("low")
-        p.ChangeDutyCycle(25)
+        pA.ChangeDutyCycle(25)
+        pB.ChangeDutyCycle(25)
         x='z'
 
     elif x=='m':
         print("medium")
-        p.ChangeDutyCycle(50)
+        pA.ChangeDutyCycle(50)
+        pB.ChangeDutyCycle(50)
         x='z'
 
     elif x=='h':
         print("high")
-        p.ChangeDutyCycle(75)
+        pA.ChangeDutyCycle(75)
+        pB.ChangeDutyCycle(75)
         x='z'
      
     
@@ -85,4 +110,4 @@ while(1):
     
     else:
         print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+        print("please enter the defined data t
